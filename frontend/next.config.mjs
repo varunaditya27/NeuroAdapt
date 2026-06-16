a34be7f54318d6a14fc7e75b84dbaa0f701f1685
Sudarshan <imsukudu24@gmail.com>
2026-06-16 10:44:55 +0530

    small config changes for dockerisation

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        destination: `${process.env.BACKEND_URL || 'http://localhost:8000'}/api/:path*`,
       },
     ];
   },
